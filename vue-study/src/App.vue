303bdd3b93cfadf3aacb15891c1d0f1bb4cf03e5
panyc <pycitol@sina.com>
2020-02-13 02:52:25 +0800

    修改normalize引入方式

--- vue-study/src/App.vue
+++ vue-study/src/App.vue
@@ -5,3 +5,4 @@
 </template>
 
 <style lang="scss"></style>
+<style></style>
--- vue-study/src/views/login/index.vue
+++ vue-study/src/views/login/index.vue
@@ -1,24 +1,64 @@
 <template>
   <div class="login">
-    <h1>This is an login page</h1>
-    <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-row>
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li
+          v-for="item in menuTab"
+          :key="item.id"
+          :class="{ current: item.current }"
+          @click="toggleMenu(item)"
+        >
+          {{ item.txt }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      menuTab: [
+        { txt: "登录", current: true },
+        { txt: "注册", current: false }
+      ],
+      isActive: true
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    toggleMenu(data) {
+      this.menuTab.forEach(element => {
+        element.current = false;
+      });
+      data.current = true;
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
-#login {
+.login {
   height: 100vh;
   background-color: #5f3441;
+}
+.login-wrap {
+  width: 330px;
+  margin: auto;
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #ffffff;
+    border-radius: 2px;
+  }
+  .current {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
 }
 </style>
